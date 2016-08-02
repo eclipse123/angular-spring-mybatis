a5d890e3435e1d95b1fb429b87a2970c649bdc55
hewy <hewy@DESKTOP-JTA3IQN>
2016-08-02 16:08:39 +0800

    数据库脚本

---
+++ db.sql
@@ -0,0 +1,10 @@
+create databese <databasename>
+create table t_user(
+ id int primary key auto_increment,
+ name varchar(20),
+ age  int,
+ password varchar(50),
+ email varchar(50),
+ icon  varchar(50)
+ 
+)
